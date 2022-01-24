f8af194fcd692c456ac67b05484414c7225723a9
Kaltenbach, Robin <robin@cskaltenbach.de>
2022-01-24 22:15:40 +0100

    Init Solution

---
+++ src/HomeOfficeManagement/HomeOfficeManagement.Core/CalendarServiceFacade.cs
@@ -0,0 +1,20 @@
+﻿using Google.Apis.Services;
+
+namespace HomeOfficeManagement.Core
+{
+    public class CalendarServiceFacade
+    {
+        public static void Test()
+        {
+            var i = new BaseClientService.Initializer();
+            i.ApiKey = "xxxx";
+
+            var d = new Google.Apis.Calendar.v3.CalendarService();
+            
+            //d.ApiKey = "AIzaSyD50U-7YnP2YlwPhqv3QK30SusR1v7rk_c";
+            var x = d.Calendars.Get("xxxx");
+
+            var y = d.Events.List("xxxx");
+        }
+    }
+}
---
+++ src/HomeOfficeManagement/HomeOfficeManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using HomeOfficeManagement.Core;
+
+namespace HomeOfficeManagement
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalendarServiceFacade.Test();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
