c4be0db356a4f3c93eb8cec23e606cdc6eb3ee8d
Kaltenbach, Robin <robin@cskaltenbach.de>
2022-01-24 22:18:45 +0100

    Deleted

--- src/HomeOfficeManagement/HomeOfficeManagement.Core/CalendarServiceFacade.cs
+++ src/HomeOfficeManagement/HomeOfficeManagement.Core/CalendarServiceFacade.cs
@@ -10,8 +10,6 @@ namespace HomeOfficeManagement.Core
             i.ApiKey = "xxxx";
 
             var d = new Google.Apis.Calendar.v3.CalendarService();
-            
-            //d.ApiKey = "AIzaSyD50U-7YnP2YlwPhqv3QK30SusR1v7rk_c";
             var x = d.Calendars.Get("xxxx");
 
             var y = d.Events.List("xxxx");
