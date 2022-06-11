23f8c32acab9a91a623b03deb67a8399d53841a6
Kaltenbach, Robin <robin@cskaltenbach.de>
2022-06-11 13:53:48 +0200

    Interfaces added

---
+++ src/HomeOfficeManagement/HomeOfficeManagement.Core/Services/Interfaces/IFritzBoxService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HomeOfficeManagement.Core.Models;
+using HomeOfficeManagement.Core.Models.FritzBox;
+
+namespace HomeOfficeManagement.Core.Services.Interfaces
+{
+    public interface IFritzBoxService
+    {
+        Task<string> GetSessionId(string benutzername, string kennwort);
+        Task<Geraeteliste> GetAlleGeraete(string sessionId);
+        Task<Geraet> GetGeraet(string sessionId, string identifier);
+        Task<Gruppe> GetGruppeByGeraet(Geraet device, string sessionId);
+
+        Task<bool> SetTemplateByHomeOffice(Gruppe deviceGruppe, IEnumerable<HomeOfficeDTO> wocheneinstellung,
+            string sessionId);
+
+        Task<Vorlagenliste> GetVorlagen(string sessionId);
+    }
+}
