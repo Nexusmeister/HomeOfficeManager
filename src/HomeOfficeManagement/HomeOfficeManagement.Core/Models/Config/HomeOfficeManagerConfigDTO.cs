e6843f95a808e8378f012441ec95b14ed5dd31ec
Kaltenbach, Robin <robin@cskaltenbach.de>
2022-06-11 14:07:04 +0200

    New Prop in config

--- src/HomeOfficeManagement/HomeOfficeManagement.Core/Models/Config/HomeOfficeManagerConfigDTO.cs
+++ src/HomeOfficeManagement/HomeOfficeManagement.Core/Models/Config/HomeOfficeManagerConfigDTO.cs
@@ -1,5 +1,6 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace HomeOfficeManagement.Core.Models.Config
 {
@@ -9,5 +10,6 @@ namespace HomeOfficeManagement.Core.Models.Config
         public ClientSecrets ClientSecrets { get; set; }
         public FritzBoxDTO FritzBox { get; set; }
         public KalenderConfigDTO Kalender { get; set; }
+        public IEnumerable<string> Zielgeraete { get; set; }
     }
 }
